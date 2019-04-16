f7b217206c378ad2921838789b118706f46bf760
Valentyn Berezin <valentyn.berezin@adorsys.com.ua>
2019-04-16 19:59:14 +0300

    Added shrinker

--- docs/modular/shrink.groovy
+++ docs/modular/shrink.groovy
@@ -1,9 +1,88 @@
-import com.google.common.base.Charsets
-import com.google.common.io.Files
-
+import java.nio.file.Files
 import java.nio.file.Paths
 
-def lines = Files.readLines(
-        Paths.get('/Users/valentyn.berezin/IdeaProjects/my-fork-datasafe/docs/modular/create_user.puml').toFile(),
-        Charsets.UTF_8
-)
+REGEX_MAPPING = [
+        'de\\.adorsys\\.datasafe\\.business\\.impl\\.profile.+': 'Profile',
+        'de\\.adorsys\\.datasafe\\.business\\.impl\\.credentials.+': 'Credentials',
+        'de\\.adorsys\\.datasafe\\.business\\.impl\\.cmsencryption.+': 'CMSEncryption',
+        'de\\.adorsys\\.datasafe\\.business\\.impl\\.dfs.+': 'DFS',
+        'de\\.adorsys\\.datasafe\\.business\\.impl\\.document.+': 'Document',
+        'de\\.adorsys\\.datasafe\\.business\\.impl\\.inbox.+': 'INBOX',
+        'de\\.adorsys\\.datasafe\\.business\\.impl\\.private.+': 'PRIVATE',
+        'de\\.adorsys\\.datasafe\\.business\\.impl\\.serde.+': 'SERDE',
+        '.+deployment\\.profile.+': 'Profile',
+        '.+deployment\\.credentials.+': 'Credentials',
+        '.+deployment\\.cmsencryption.+': 'CMSEncryption',
+        '.+deployment\\.dfs.+': 'DFS',
+        '.+deployment\\.document.+': 'Document',
+        '.+deployment\\.inbox.+': 'INBOX',
+        '.+deployment\\.private.+': 'PRIVATE',
+        '.+deployment\\.serde.+': 'SERDE'
+]
+
+def lines = Files.lines(
+        Paths.get('/Users/valentyn.berezin/IdeaProjects/my-fork-datasafe/docs/modular/create_user.puml')
+).collect {it}
+
+def classFromPackage(String pkg) {
+    String[] parts = pkg.split('\\.')
+    return parts.length > 1 ? parts[parts.length - 1] : pkg
+}
+
+fqn = [:]
+
+lines.forEach {
+    if (it.startsWith("' ")) {
+        def packageName = it.substring(2)
+        fqn[classFromPackage(packageName)] = packageName
+    }
+}
+
+def mappedLines = []
+
+String replacement(String clazz) {
+
+    for (def it : REGEX_MAPPING) {
+        if (clazz.matches(it.key)) {
+            return it.value
+        }
+    }
+
+    return null
+}
+
+def replaceIfNeeded(String line, String sep) {
+    if (!line.contains(sep)) {
+        return line
+    }
+
+    def parts = line.split(sep)
+
+    def lhs = parts[0].replaceAll('[- ]', '')
+    def rhs = parts[1].split(':')[0].replaceAll('[- ]', '')
+
+    if (fqn[lhs]) {
+        def repl = replacement(fqn[lhs])
+        if (repl) {
+            line = line.replaceAll("\\b${lhs}\\b", repl)
+        }
+    }
+
+    if (fqn[rhs]) {
+        def repl = replacement(fqn[rhs])
+        if (repl) {
+            line = line.replaceAll("\\b${rhs}\\b", repl)
+        }
+    }
+
+    return line
+}
+
+lines.forEach {
+    def line = replaceIfNeeded(it, '<')
+    line = replaceIfNeeded(line, '>')
+
+    mappedLines += line
+}
+
+print(mappedLines.join('\n'))
