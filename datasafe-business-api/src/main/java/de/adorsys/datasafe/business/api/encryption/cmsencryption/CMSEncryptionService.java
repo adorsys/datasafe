da7770c5b7b56db1e342d7dcbded7403bab9506c
fpo <fpo@adorsys.de>
2019-04-19 00:22:17 +0200

    Moving Stream encryption to CMSEncryptionService

--- datasafe-business-api/src/main/java/de/adorsys/datasafe/business/api/encryption/cmsencryption/CMSEncryptionService.java
+++ datasafe-business-api/src/main/java/de/adorsys/datasafe/business/api/encryption/cmsencryption/CMSEncryptionService.java
@@ -2,9 +2,12 @@ package de.adorsys.datasafe.business.api.encryption.cmsencryption;
 
 import de.adorsys.datasafe.business.api.deployment.keystore.types.KeyID;
 import de.adorsys.datasafe.business.api.deployment.keystore.types.KeyStoreAccess;
+import de.adorsys.datasafe.business.api.deployment.keystore.types.PublicKeyIDWithPublicKey;
 import de.adorsys.datasafe.business.api.types.DocumentContent;
 import org.bouncycastle.cms.CMSEnvelopedData;
 
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.security.PublicKey;
 
 public interface CMSEncryptionService {
@@ -12,4 +15,8 @@ public interface CMSEncryptionService {
     CMSEnvelopedData encrypt(DocumentContent data, PublicKey publicKey, KeyID publicKeyID);
 
     DocumentContent decrypt(CMSEnvelopedData cmsEnvelopedData, KeyStoreAccess keyStoreAccess);
+
+    OutputStream buildEncryptionOutputStream(OutputStream dataContentStream, PublicKey publicKey, KeyID publicKeyID);
+
+    InputStream buildDecryptionInputStream(InputStream inputStream, KeyStoreAccess keyStoreAccess);
 }
--- datasafe-business-impl/src/main/java/de/adorsys/datasafe/business/impl/cmsencryption/services/CMSEncryptionServiceImpl.java
+++ datasafe-business-impl/src/main/java/de/adorsys/datasafe/business/impl/cmsencryption/services/CMSEncryptionServiceImpl.java
@@ -1,13 +1,23 @@
 package de.adorsys.datasafe.business.impl.cmsencryption.services;
 
-import de.adorsys.datasafe.business.api.encryption.cmsencryption.CMSEncryptionService;
-import de.adorsys.datasafe.business.api.deployment.keystore.types.KeyID;
-import de.adorsys.datasafe.business.api.deployment.keystore.types.KeyStoreAccess;
-import de.adorsys.datasafe.business.api.types.DocumentContent;
-import de.adorsys.datasafe.business.impl.cmsencryption.exceptions.AsymmetricEncryptionException;
-import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
-import org.bouncycastle.cms.*;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Iterator;
+
+import javax.inject.Inject;
+
+import org.bouncycastle.cms.CMSAlgorithm;
+import org.bouncycastle.cms.CMSEnvelopedData;
+import org.bouncycastle.cms.CMSEnvelopedDataGenerator;
+import org.bouncycastle.cms.CMSEnvelopedDataParser;
+import org.bouncycastle.cms.CMSEnvelopedDataStreamGenerator;
+import org.bouncycastle.cms.CMSProcessableByteArray;
+import org.bouncycastle.cms.CMSTypedData;
+import org.bouncycastle.cms.KeyTransRecipientId;
+import org.bouncycastle.cms.RecipientInformation;
+import org.bouncycastle.cms.RecipientInformationStore;
 import org.bouncycastle.cms.jcajce.JceCMSContentEncryptorBuilder;
 import org.bouncycastle.cms.jcajce.JceKeyTransEnvelopedRecipient;
 import org.bouncycastle.cms.jcajce.JceKeyTransRecipient;
@@ -15,10 +25,13 @@ import org.bouncycastle.cms.jcajce.JceKeyTransRecipientInfoGenerator;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.operator.OutputEncryptor;
 
-import javax.inject.Inject;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.util.Iterator;
+import de.adorsys.datasafe.business.api.deployment.keystore.types.KeyID;
+import de.adorsys.datasafe.business.api.deployment.keystore.types.KeyStoreAccess;
+import de.adorsys.datasafe.business.api.encryption.cmsencryption.CMSEncryptionService;
+import de.adorsys.datasafe.business.api.types.DocumentContent;
+import de.adorsys.datasafe.business.impl.cmsencryption.exceptions.AsymmetricEncryptionException;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Cryptographic message syntax document encoder/decoder - see
@@ -81,4 +94,45 @@ public class CMSEncryptionServiceImpl implements CMSEncryptionService {
 
         return new DocumentContent(recipientInfo.getContent(recipient));
     }
+    
+    @Override
+    @SneakyThrows
+    public OutputStream buildEncryptionOutputStream(OutputStream dataContentStream, PublicKey publicKey, KeyID publicKeyID) {
+        CMSEnvelopedDataStreamGenerator gen = new CMSEnvelopedDataStreamGenerator();
+
+        gen.addRecipientInfoGenerator(new JceKeyTransRecipientInfoGenerator(
+        		publicKeyID.getValue().getBytes(),
+                publicKey
+        ));
+
+        return gen.open(dataContentStream, new JceCMSContentEncryptorBuilder(CMSAlgorithm.AES128_CBC)
+                .setProvider(BouncyCastleProvider.PROVIDER_NAME)
+                .build());
+    }
+
+    @Override
+    @SneakyThrows
+    public InputStream buildDecryptionInputStream(InputStream inputStream, KeyStoreAccess keyStoreAccess) {
+        RecipientInformationStore recipientInfoStore = new CMSEnvelopedDataParser(inputStream).getRecipientInfos();
+
+        if(recipientInfoStore.size() == 0) {
+            throw new AsymmetricEncryptionException("CMS Envelope doesn't contain recipients");
+        }
+        if(recipientInfoStore.size() > 1) {
+            throw new AsymmetricEncryptionException("Programming error. Handling of more that one recipient not done yet");
+        }
+        RecipientInformation recipientInfo = recipientInfoStore.getRecipients().stream().findFirst().get();
+        KeyTransRecipientId recipientId = (KeyTransRecipientId) recipientInfo.getRID();
+        byte[] subjectKeyIdentifier = recipientId.getSubjectKeyIdentifier();
+        String keyId = new String(subjectKeyIdentifier);
+        log.debug("Private key ID from envelope: {}", keyId);
+
+        PrivateKey privateKey = (PrivateKey) keyStoreAccess.getKeyStore().getKey(
+                keyId,
+                keyStoreAccess.getKeyStoreAuth().getReadKeyPassword().getValue().toCharArray()
+        );
+
+        return recipientInfo.getContentStream(new JceKeyTransEnvelopedRecipient(privateKey)).getContentStream();
+    }
+    
 }
