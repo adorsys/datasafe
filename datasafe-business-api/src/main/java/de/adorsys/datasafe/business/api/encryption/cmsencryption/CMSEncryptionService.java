5a170e0aba641005cf97d34ce3cef0def2d31829
Ihor Yatsenko <yatsenko.ihor@gmail.com>
2019-04-22 10:51:17 +0300

    DOC-105: Write a positive test with large document for CMS Encryption

--- datasafe-business-api/src/main/java/de/adorsys/datasafe/business/api/encryption/cmsencryption/CMSEncryptionService.java
+++ datasafe-business-api/src/main/java/de/adorsys/datasafe/business/api/encryption/cmsencryption/CMSEncryptionService.java
@@ -13,7 +13,7 @@ public interface CMSEncryptionService {
 
     OutputStream buildEncryptionOutputStream(OutputStream dataContentStream, PublicKey publicKey, KeyID publicKeyID);
 
-    OutputStream buildEncryptionOutputStream(OutputStream dataContentStream, SecretKey secretKey, KeyID keyID);
+    OutputStream buildEncryptionOutputStream(OutputStream dataContentStream, SecretKey secretKey, KeyID secretKeyID);
     
     InputStream buildDecryptionInputStream(InputStream inputStream, KeyStoreAccess keyStoreAccess);
 }
