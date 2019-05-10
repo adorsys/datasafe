7a84715c0324f21c2cb01c6540e4928e8b825d56
Valentyn Berezin <valentyn.berezin@adorsys.com.ua>
2019-05-10 19:41:58 +0300

    DOC-111. Added bucket access service test

---
+++ datasafe-directory/datasafe-directory-impl/src/test/java/de/adorsys/datasafe/business/impl/profile/dfs/BucketAccessServiceImplTest.java
@@ -0,0 +1,40 @@
+package de.adorsys.datasafe.business.impl.profile.dfs;
+
+import de.adorsys.datasafe.business.api.types.UserID;
+import de.adorsys.datasafe.business.api.types.UserIDAuth;
+import de.adorsys.datasafe.business.api.types.keystore.ReadKeyPassword;
+import de.adorsys.datasafe.business.api.types.resource.DefaultPrivateResource;
+import de.adorsys.datasafe.business.api.types.resource.DefaultPublicResource;
+import de.adorsys.datasafe.shared.BaseMockitoTest;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BucketAccessServiceImplTest extends BaseMockitoTest {
+
+    private static final String ABSOLUTE_BUCKET = "s3://bucket";
+
+    private UserIDAuth auth = new UserIDAuth(new UserID(""), new ReadKeyPassword(""));
+
+    @InjectMocks
+    private BucketAccessServiceImpl bucketAccessService;
+
+    @Test
+    void privateAccessFor() {
+        assertThat(bucketAccessService.privateAccessFor(
+                auth,
+                DefaultPrivateResource.forPrivate(URI.create(ABSOLUTE_BUCKET))).location()
+        ).asString().isEqualTo(ABSOLUTE_BUCKET);
+    }
+
+    @Test
+    void publicAccessFor() {
+        assertThat(bucketAccessService.publicAccessFor(
+                auth.getUserID(),
+                DefaultPublicResource.forAbsolutePublic(URI.create(ABSOLUTE_BUCKET))).location()
+        ).asString().isEqualTo(ABSOLUTE_BUCKET);
+    }
+}
