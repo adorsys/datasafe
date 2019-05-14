cd9e92c86f03132120e3022727a1247d52cb8e87
Valentyn Berezin <valentyn.berezin@adorsys.com.ua>
2019-05-14 17:00:13 +0300

    DOC-135. Removed unnecessary Inject

--- datasafe-metainfo-version/datasafe-metainfo-version-impl/src/main/java/de/adorsys/datasafe/business/impl/version/types/LatestDFSVersion.java
+++ datasafe-metainfo-version/datasafe-metainfo-version-impl/src/main/java/de/adorsys/datasafe/business/impl/version/types/LatestDFSVersion.java
@@ -1,13 +1,7 @@
 package de.adorsys.datasafe.business.impl.version.types;
 
-import javax.inject.Inject;
-
 /**
  * Latest version that is known for DFS system.
  */
 public class LatestDFSVersion implements DFSVersionStrategy {
-
-    @Inject
-    public LatestDFSVersion() {
-    }
 }
