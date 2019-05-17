4e68829c2d50f6533fc468d419c48919f4700146
Maxim Grischenko <grischenko.maxim@gmail.com>
2019-05-17 18:24:26 +0300

    DOC-141 OpenShift deployment configuration

--- datasafe-rest/src/main/java/de/adorsys/datasafe/rest/DatasafeRestApplication.java
+++ datasafe-rest/src/main/java/de/adorsys/datasafe/rest/DatasafeRestApplication.java
@@ -1,4 +1,4 @@
-package de.adorsys.datasafe.rest.config;
+package de.adorsys.datasafe.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
