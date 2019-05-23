8b6bb2cbfe0843373998cc2aad1a8dde1d35098f
Valentyn Berezin <valentyn.berezin@adorsys.com.ua>
2019-05-23 18:53:21 +0300

    DOC-153. Added ceph

--- datasafe-business/src/test/java/de/adorsys/datasafe/business/impl/e2e/performance/CephPerformance.java
+++ datasafe-business/src/test/java/de/adorsys/datasafe/business/impl/e2e/performance/CephPerformance.java
@@ -1,4 +1,32 @@
 package de.adorsys.datasafe.business.impl.e2e.performance;
 
-public class CephPerformance {
+import de.adorsys.datasafe.business.impl.e2e.DatasafeServicesProvider;
+import de.adorsys.datasafe.business.impl.e2e.performance.services.OperationExecutor;
+import de.adorsys.datasafe.business.impl.e2e.performance.services.StatisticService;
+import de.adorsys.datasafe.business.impl.service.DefaultDatasafeServices;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class CephPerformance extends WithRandomActionPerformance {
+
+    @ParameterizedTest
+    @MethodSource("sizesAndLatency")
+    @EnabledIfEnvironmentVariable(named = "CEPH_PERFORMANCE_TEST", matches = "true")
+    void testCephBucketPerformance(int size, int latency) {
+        DelegatingStorageWithDelay service = storageService(ceph(), latency);
+        DefaultDatasafeServices services = DatasafeServicesProvider.defaultDatasafeServices(
+                service, service.getRootLocation());
+        initialize(services);
+        initUsers(size);
+
+        OperationExecutor executor = new OperationExecutor(
+                services.privateService(),
+                services.inboxService(),
+                users,
+                STATS.computeIfAbsent(named("CEPH ", size, latency), id -> new StatisticService())
+        );
+
+        executeOperations(executor);
+    }
 }
