0901ac62df1d1a40ac1b76249df65c067ce7d726
Valentyn Berezin <valentyn.berezin@adorsys.com.ua>
2019-06-25 14:59:58 +0300

    DOC-184. Update codecov script

--- .travis/codecov_bash.sh
+++ .travis/codecov_bash.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
-if [ “$TRAVIS_PULL_REQUEST_BRANCH” == “$TRAVIS_BRANCH” ]; then bash <(curl -s https://codecov.io/bash) -f '!*datasafe-business*' -F unittests; bash <(curl -s https://codecov.io/bash) -f '*datasafe-business*' -F e2e_tests; fi
+
+BRANCH="$TRAVIS_BRANCH"
+if [[ -n "$TRAVIS_PULL_REQUEST_BRANCH" ]]; then
+    BRANCH="$TRAVIS_PULL_REQUEST_BRANCH"
+    echo "Pull request branch identified: $TRAVIS_PULL_REQUEST_BRANCH"
+fi
+
+echo "Sending test results to codecov using $BRANCH"
+bash <(curl -s https://codecov.io/bash) -f '!*datasafe-business*' -F unittests -B "$BRANCH";
+bash <(curl -s https://codecov.io/bash) -f '*datasafe-business*' -F e2e_tests  -B "$BRANCH";
