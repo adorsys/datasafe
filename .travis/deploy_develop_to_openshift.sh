4a0b28d92a471be45d621f5ab51d477e9ca67642
Valentyn Berezin <valentyn.berezin@adorsys.com.ua>
2019-06-26 16:22:17 +0300

    DOC-209. Disable OC tag

--- .travis/deploy_develop_to_openshift.sh
+++ .travis/deploy_develop_to_openshift.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
 PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 oc login https://openshift.adorsys.de:443 --username=$OPENSHIFT_USER --password=$OPENSHIFT_PASSWORD
+
+# this should be fixed by DOC-209
+IMAGE_TAG="latest"
+
 docker login -u $(oc whoami) -p $(oc whoami -t) https://openshift-registry.adorsys.de
-docker build -t datasafe-rest-service:$TRAVIS_COMMIT --build-arg JAR_FILE=datasafe-rest-impl-$PROJECT_VERSION.jar ./datasafe-rest-impl
+docker build -t datasafe-rest-service:$IMAGE_TAG --build-arg JAR_FILE=datasafe-rest-impl-$PROJECT_VERSION.jar ./datasafe-rest-impl
 export IMAGE_NAME=openshift-registry.adorsys.de/datasafe/datasafe-rest-service
-docker tag datasafe-rest-service:$TRAVIS_COMMIT $IMAGE_NAME:$TRAVIS_COMMIT
-docker push $IMAGE_NAME:$TRAVIS_COMMIT
+docker tag datasafe-rest-service:$IMAGE_TAG $IMAGE_NAME:$IMAGE_TAG
+docker push $IMAGE_NAME:$IMAGE_TAG
 
-# check if image was pushed successfully
-echo '{"experimental": "enabled"}' > ~/.docker/config.json \
-    && docker manifest inspect $IMAGE_NAME:$TRAVIS_COMMIT > /dev/null
-
-status=$?
-if [[ ${status} -ne 0 ]]; then
-    echo "ERROR, no deploy will be done, push image failed"
-else
-    echo "Image pushed successfully"
-    oc tag $IMAGE_NAME:$TRAVIS_COMMIT datasafe-rest-service:latest
-fi
+# Should be done by DOC-209
+# oc tag $IMAGE_NAME:$IMAGE_TAG datasafe-rest-service:latest
