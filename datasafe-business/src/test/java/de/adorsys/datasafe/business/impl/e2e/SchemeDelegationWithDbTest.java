5833777ca93b8332aeae9fd27ffcaca06a4d3598
Valentyn Berezin <valentyn.berezin@aurea.com>
2019-06-27 09:14:23 +0300

    DOC-180. Updated tests due to layout change due to merge

--- datasafe-business/src/test/java/de/adorsys/datasafe/business/impl/e2e/SchemeDelegationWithDbTest.java
+++ datasafe-business/src/test/java/de/adorsys/datasafe/business/impl/e2e/SchemeDelegationWithDbTest.java
@@ -11,6 +11,7 @@ import de.adorsys.datasafe.storage.api.StorageService;
 import de.adorsys.datasafe.storage.impl.db.DatabaseConnectionRegistry;
 import de.adorsys.datasafe.storage.impl.db.DatabaseCredentials;
 import de.adorsys.datasafe.storage.impl.db.DatabaseStorageService;
+import de.adorsys.datasafe.storage.impl.fs.FileSystemStorageService;
 import de.adorsys.datasafe.types.api.actions.WriteRequest;
 import de.adorsys.datasafe.types.api.resource.AbsoluteLocation;
 import de.adorsys.datasafe.types.api.resource.BasePrivateResource;
@@ -18,11 +19,15 @@ import de.adorsys.datasafe.types.api.resource.Uri;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import org.testcontainers.shaded.com.google.common.collect.ImmutableMap;
 
 import java.io.OutputStream;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,33 +36,32 @@ class SchemeDelegationWithDbTest extends WithStorageProvider {
 
     private static final Set<String> ALLOWED_TABLES = ImmutableSet.of("users", "private_profiles", "public_profiles");
 
-    private Uri minioPath;
-    private StorageService minio;
+    private Path fsPath;
+    private StorageService filesystem;
     private StorageService db;
     private DefaultDatasafeServices datasafeServices;
 
     @BeforeEach
-    void initialize() {
-        StorageDescriptor minioDescriptor = minio();
-        this.minio = minioDescriptor.getStorageService().get();
-        this.minioPath = minioDescriptor.getLocation();
+    void initialize(@TempDir Path tempDir) {
+        this.fsPath = tempDir;
+        this.filesystem = new FileSystemStorageService(tempDir);
         this.db = new DatabaseStorageService(ALLOWED_TABLES, new DatabaseConnectionRegistry(
-                uri -> uri.location().getWrapped().getScheme() + ":" + uri.location().getPath().split("/")[1],
-                ImmutableMap.of("jdbc://localhost:9999", new DatabaseCredentials("sa", "sa")))
+            uri -> uri.location().getWrapped().getScheme() + ":" + uri.location().getPath().split("/")[1],
+            ImmutableMap.of("jdbc://localhost:9999", new DatabaseCredentials("sa", "sa")))
         );
 
         StorageService multiDfs = new SchemeDelegatingStorage(
-                ImmutableMap.of(
-                        "s3", minio,
-                        "jdbc", db
-                )
+            ImmutableMap.of(
+                "file", filesystem,
+                "jdbc", db
+            )
         );
 
         this.datasafeServices = DaggerDefaultDatasafeServices
-                .builder()
-                .config(new ProfilesOnDbDataOnMinio(minioPath.asURI(), URI.create("jdbc://localhost:9999/h2:mem:test/")))
-                .storage(multiDfs)
-                .build();
+            .builder()
+            .config(new ProfilesOnDbDataOnFs(tempDir.toUri(), URI.create("jdbc://localhost:9999/h2:mem:test/")))
+            .storage(multiDfs)
+            .build();
     }
 
     @Test
@@ -70,36 +74,44 @@ class SchemeDelegationWithDbTest extends WithStorageProvider {
 
         // But this data - it will be saved to FS
         try (OutputStream os =
-                     datasafeServices.privateService().write(WriteRequest.forDefaultPrivate(userJohn, "file.txt"))) {
+                 datasafeServices.privateService().write(WriteRequest.forDefaultPrivate(userJohn, "file.txt"))) {
             os.write("Hello".getBytes());
         }
 
         // Profiles are on DB
         assertThat(listDb("jdbc://localhost:9999/h2:mem:test/private_profiles/"))
-                .containsExactly("jdbc://localhost:9999/h2:mem:test/private_profiles/john");
+            .containsExactly("jdbc://localhost:9999/h2:mem:test/private_profiles/john");
         assertThat(listDb("jdbc://localhost:9999/h2:mem:test/public_profiles/"))
-                .containsExactly("jdbc://localhost:9999/h2:mem:test/public_profiles/john");
-        
+            .containsExactly("jdbc://localhost:9999/h2:mem:test/public_profiles/john");
+
+        Path encryptedFile = Files.walk(fsPath.resolve("users/john/private/files/")).collect(Collectors.toList()).get(1);
         // File and keystore/pub keys are on FS
-        // File and keystore/pub keys are on minio
-        assertThat(minio.list(new AbsoluteLocation<>(BasePrivateResource.forPrivate(minioPath.resolve("")))))
-            .extracting(it -> minioPath.relativize(it.location()))
-            .extracting(it -> it.asURI().toString())
-            .contains("john/private/keystore", "john/public/pubkeys")
-            .anyMatch(it -> it.startsWith("john/private/files/"))
-            .hasSize(3);
+        assertThat(Files.walk(fsPath))
+            .extracting(it -> fsPath.relativize(it))
+            .extracting(Path::toString)
+            .containsExactlyInAnyOrder(
+                "",
+                "users",
+                "users/john",
+                "users/john/public",
+                "users/john/public/pubkeys",
+                "users/john/private",
+                "users/john/private/keystore",
+                "users/john/private/files",
+                fsPath.relativize(encryptedFile).toString()
+            );
     }
 
     private Stream<String> listDb(String path) {
         return db.list(BasePrivateResource.forAbsolutePrivate(URI.create(path)))
-                .map(it -> it.location().asURI().toString());
+            .map(it -> it.location().asURI().toString());
     }
 
-    static class ProfilesOnDbDataOnMinio extends DefaultDFSConfig {
+    static class ProfilesOnDbDataOnFs extends DefaultDFSConfig {
 
         private final Uri profilesPath;
 
-        ProfilesOnDbDataOnMinio(URI fsPath, URI profilesPath) {
+        ProfilesOnDbDataOnFs(URI fsPath, URI profilesPath) {
             super(fsPath, "PAZZWORT");
             this.profilesPath = new Uri(profilesPath);
         }
@@ -107,18 +119,18 @@ class SchemeDelegationWithDbTest extends WithStorageProvider {
         @Override
         public AbsoluteLocation publicProfile(UserID forUser) {
             return new AbsoluteLocation<>(
-                    BasePrivateResource.forPrivate(
-                            profilesPath.resolve("public_profiles/").resolve(forUser.getValue())
-                    )
+                BasePrivateResource.forPrivate(
+                    profilesPath.resolve("public_profiles/").resolve(forUser.getValue())
+                )
             );
         }
 
         @Override
         public AbsoluteLocation privateProfile(UserID forUser) {
             return new AbsoluteLocation<>(
-                    BasePrivateResource.forPrivate(
-                            profilesPath.resolve("private_profiles/").resolve(forUser.getValue())
-                    )
+                BasePrivateResource.forPrivate(
+                    profilesPath.resolve("private_profiles/").resolve(forUser.getValue())
+                )
             );
         }
     }
