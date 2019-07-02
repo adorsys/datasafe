cfe6adf70af9a71f71e0251f288f9e0834a833c1
Valentyn Berezin <valentyn.berezin@adorsys.com.ua>
2019-07-02 15:54:13 +0300

    DOC-205. Fixed regex value

--- datasafe-types-api/src/main/java/de/adorsys/datasafe/types/api/resource/UriEncoderDecoder.java
+++ datasafe-types-api/src/main/java/de/adorsys/datasafe/types/api/resource/UriEncoderDecoder.java
@@ -22,7 +22,7 @@ import java.util.stream.Collectors;
 public class UriEncoderDecoder {
 
     // naive approach, one should really use URI builder
-    private static final Pattern SEGMENTS = Pattern.compile("(?<hostWithAuth>.+:///*[^/]+/*)*(<path>.*)");
+    private static final Pattern SEGMENTS = Pattern.compile("(?<hostWithAuth>.+:///*[^/]+/*)*(?<path>.*)");
 
     public URI encode(String uri) {
         return encode(uri, UriEncoderDecoder::encodeSegment);
