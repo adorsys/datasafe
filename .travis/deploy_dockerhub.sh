cab95c87efef4a3314fe8d7e6726bd596a959b2b
Maxim Grischenko <grischenko.maxim@gmail.com>
2019-07-02 17:08:03 +0200

    deploy dockerhub

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
-docker build -t datasafe-rest-service:$IMAGE_TAG --build-arg JAR_FILE=datasafe-rest-impl-$PROJECT_VERSION.jar ./datasafe-rest-impl
-docker login -u $DOCKER_USER -p $DOCKER_PASS
-IMAGE_NAME=adorsys/datasafe-rest-service
+IMAGE_NAME=max402/datasafe
 IMAGE_TAG=${TRAVIS_COMMIT:0:7}
-docker push $IMAGE_NAME:$IMAGE_TAG
-oc tag $IMAGE_NAME:$IMAGE_TAG datasafe-rest-service:latest
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+
+docker build -t $IMAGE_NAME:$IMAGE_TAG --build-arg JAR_FILE=datasafe-rest-impl-$PROJECT_VERSION.jar ./datasafe-rest-impl
+docker push $IMAGE_NAME:$IMAGE_TAG || exit 1
+
+docker tag $IMAGE_NAME:$IMAGE_TAG $IMAGE_NAME:latest
+docker push $IMAGE_NAME:latest || exit 1
+
+oc login https://openshift.adorsys.de:443 --username=$OPENSHIFT_USER --password=$OPENSHIFT_PASSWORD
+oc import-image datasafe
