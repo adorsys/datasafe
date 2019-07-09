69bf35b679e24773712549770636fbfc2ac993aa
Valentyn Berezin <valentyn.berezin@aurea.com>
2019-07-09 21:45:09 +0300

    DOC-216. Conditional frontend exposure

--- datasafe-rest-impl/run.sh
+++ datasafe-rest-impl/run.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
-# Bind API url and credentials
-sed -i 's!${API_URL}!'"$API_URL"'!g' "$APP_HOME/frontend/env.js"
-sed -i 's!${API_USERNAME}!'"$API_USERNAME"'!g' "$APP_HOME/frontend/env.js"
-sed -i 's!${API_PASSWORD}!'"$API_PASSWORD"'!g' "$APP_HOME/frontend/env.js"
+EXTRA_ARGS=""
 
-java -DSTATIC_RESOURCES="file://$APP_HOME/frontend/" -jar "$JAR_FILE"
+# frontend will be available only if $ENABLE_FRONTEND is set
+if [[ "$ENABLE_FRONTEND" == "true" ]]; then
+
+    if [[ -z "$API_URL" ]]; then
+        API_URL="http://localhost:8080"
+    fi
+
+    # Bind API url and credentials
+    sed -i 's!${API_URL}!'"$API_URL"'!g' "$APP_HOME/frontend/env.js"
+    sed -i 's!${API_USERNAME}!'"$DEFAULT_USER"'!g' "$APP_HOME/frontend/env.js"
+    sed -i 's!${API_PASSWORD}!'"$DEFAULT_PASSWORD"'!g' "$APP_HOME/frontend/env.js"
+
+    EXTRA_ARGS="-DSTATIC_RESOURCES=""file://$APP_HOME/frontend/"
+fi
+
+java "$EXTRA_ARGS" -jar "$JAR_FILE"
