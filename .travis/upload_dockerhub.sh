ac16a20b0489123fe3a98d3b5fe9e3dd92f3c50d
Maxim Grischenko <grischenko.maxim@gmail.com>
2019-07-10 09:58:47 +0200

    dockerhub only upload, deploy from openshift registry

--- .travis/upload_dockerhub.sh
+++ .travis/upload_dockerhub.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
-IMAGE_NAME=max402/datasafe
+IMAGE_NAME=adorsys/datasafe
 IMAGE_TAG=${TRAVIS_COMMIT:0:7}
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 
@@ -9,6 +9,3 @@ docker push $IMAGE_NAME:$IMAGE_TAG || exit 1
 
 docker tag $IMAGE_NAME:$IMAGE_TAG $IMAGE_NAME:latest
 docker push $IMAGE_NAME:latest || exit 1
-
-oc login https://openshift.adorsys.de:443 --username=$OPENSHIFT_USER --password=$OPENSHIFT_PASSWORD
-oc import-image datasafe
