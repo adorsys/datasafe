a1d1520bd04cb0ee96b104fa4fc52baccaff5f5c
Maxim Grischenko <grischenko.maxim@gmail.com>
2019-07-12 12:16:08 +0200

    oc tag new format

--- .travis/deploy_develop_to_openshift.sh
+++ .travis/deploy_develop_to_openshift.sh
@@ -2,10 +2,10 @@
 PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 oc login https://openshift.adorsys.de:443 --username=$OPENSHIFT_USER --password=$OPENSHIFT_PASSWORD
 IMAGE_TAG=${TRAVIS_COMMIT:0:7}
-
-docker login -u $(oc whoami) -p $(oc whoami -t) https://openshift-registry.adorsys.de
-docker build -t datasafe-rest-service:$IMAGE_TAG --build-arg JAR_FILE=datasafe-rest-impl-$PROJECT_VERSION.jar ./datasafe-rest-impl
-export IMAGE_NAME=openshift-registry.adorsys.de/datasafe/datasafe-rest-service
-docker tag datasafe-rest-service:$IMAGE_TAG $IMAGE_NAME:$IMAGE_TAG
+REGISTRY_DOMAIN=openshift-registry.adorsys.de
+SERVICE_NAME=datasafe-rest-service
+IMAGE_NAME=$REGISTRY_DOMAIN/datasafe/$SERVICE_NAME
+docker login -u $(oc whoami) -p $(oc whoami -t) https://$REGISTRY_DOMAIN
+docker build -t $IMAGE_NAME:$IMAGE_TAG --build-arg JAR_FILE=datasafe-rest-impl-$PROJECT_VERSION.jar ./datasafe-rest-impl
 docker push $IMAGE_NAME:$IMAGE_TAG && \
-oc tag $IMAGE_NAME:$IMAGE_TAG datasafe-rest-service:latest
+oc tag datasafe/$SERVICE_NAME:$IMAGE_TAG datasafe/$SERVICE_NAME:latest
