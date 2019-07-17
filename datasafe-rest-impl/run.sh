7e8510ee1a55d56fde5acdc6cdcaae4d5ae4edff
Valentyn Berezin <valentyn.berezin@adorsys.com.ua>
2019-07-17 12:04:30 +0300

    Attempt to fix frontend deployment - env vars binding

--- datasafe-rest-impl/run.sh
+++ datasafe-rest-impl/run.sh
@@ -6,8 +6,8 @@ if [[ -z "$API_URL" ]]; then
     API_URL="http://localhost:8080"
 fi
 
-# Bind API url and credentials
-sed -i 's!${API_URL}!'"$API_URL"'!g' "$APP_HOME/frontend/env.js"
+# Bind API url and credentials, sed -i won't work because of OC user permissions
+sed 's!${API_URL}!'"$API_URL"'!g' "$APP_HOME/frontend/env.js" > /tmp/env.js && mv /tmp/env.js "$APP_HOME/frontend/env.js"
 
 # do not expose sensitive data by default
 LOGIN=""
@@ -17,8 +17,8 @@ if [[ "$EXPOSE_API_CREDS" == "true" ]]; then
     PASSWORD="$DEFAULT_PASSWORD"
 fi
 
-sed -i 's!${API_USERNAME}!'"$LOGIN"'!g' "$APP_HOME/frontend/env.js"
-sed -i 's!${API_PASSWORD}!'"$PASSWORD"'!g' "$APP_HOME/frontend/env.js"
+sed 's!${API_USERNAME}!'"$LOGIN"'!g' "$APP_HOME/frontend/env.js" > /tmp/env.js && mv /tmp/env.js "$APP_HOME/frontend/env.js"
+sed 's!${API_PASSWORD}!'"$PASSWORD"'!g' "$APP_HOME/frontend/env.js" > /tmp/env.js && mv /tmp/env.js "$APP_HOME/frontend/env.js"
 
 # Expose frontend
 EXTRA_ARGS="-DSTATIC_RESOURCES=""file://$APP_HOME/frontend/"
