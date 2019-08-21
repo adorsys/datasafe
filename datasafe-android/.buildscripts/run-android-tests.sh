9ed1016ab486e1c1065d470764738266c05a158c
Drilon Reçica <drilonrecica.dev@gmail.com>
2019-08-21 20:59:20 +0200

    DAT-150 Android -Run Instrumented test on emulator

--- datasafe-android/.buildscripts/run-android-tests.sh
+++ datasafe-android/.buildscripts/run-android-tests.sh
@@ -2,16 +2,8 @@
 
 set +e
 
-#echo -e "\033[0;32m ./gradlew app:connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.class=de.adorsys.android.datasafe.DatasafeLogicTest --stacktrace \033[0m"
-#./gradlew app:connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.class=de.adorsys.android.datasafe.DatasafeLogicTest --stacktrace
-
-echo -e "\033[0;32m Android test are coming soon \033[0m"
-
 echo -e "\033[0;32m cd datasafe-android \033[0m"
 cd datasafe-android
 
-echo -e "\033[0;32m ./gradlew :datasafe:clean --stacktrace \033[0m"
-./gradlew :datasafe:clean --stacktrace
-
-echo -e "\033[0;32m ./gradlew :datasafe:assembleDebug --stacktrace \033[0m"
-./gradlew :datasafe:assembleDebug --stacktrace
+echo -e "\033[0;32m ./gradlew app:connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.class=de.adorsys.android.datasafeandroidsample.ExampleInstrumentedTest --stacktrace \033[0m"
+./gradlew app:connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.class=de.adorsys.android.datasafeandroidsample.ExampleInstrumentedTest --stacktrace
--- datasafe-android/app/src/androidTest/java/de/adorsys/android/datasafeandroidsample/ExampleInstrumentedTest.kt
+++ datasafe-android/app/src/androidTest/java/de/adorsys/android/datasafeandroidsample/ExampleInstrumentedTest.kt
@@ -1,7 +1,7 @@
 package de.adorsys.android.datasafeandroidsample
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 
 import org.junit.Test
 import org.junit.runner.RunWith
