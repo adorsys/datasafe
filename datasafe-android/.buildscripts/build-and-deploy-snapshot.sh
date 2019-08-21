498ed77d43b4bf4df6327530ad5e2d017b7f614b
Drilon Reçica <drilonrecica.dev@gmail.com>
2019-08-21 19:48:36 +0200

    DAT-150 Rename datasafe module & add buildscripts

--- datasafe-android/.buildscripts/build-and-deploy-snapshot.sh
+++ datasafe-android/.buildscripts/build-and-deploy-snapshot.sh
@@ -3,20 +3,20 @@
 echo -e "\033[0;32m cd datasafe-android \033[0m"
 cd datasafe-android
 
-echo -e "\033[0;32m ./gradlew  :datasafe-android:clean --stacktrace \033[0m"
-./gradlew :datasafe-android:clean --stacktrace
+echo -e "\033[0;32m ./gradlew  :datasafe:clean --stacktrace \033[0m"
+./gradlew :datasafe:clean --stacktrace
 
-echo -e "\033[0;32m ./gradlew :datasafe-android:assembleDebug --stacktrace \033[0m"
-./gradlew :datasafe-android:assembleDebug --stacktrace
+echo -e "\033[0;32m ./gradlew :datasafe:assembleDebug --stacktrace \033[0m"
+./gradlew :datasafe:assembleDebug --stacktrace
 
-#echo -e "\033[0;32m :datasafe-android:install --stacktrace \033[0m"
-#./gradlew :datasafe-android:install --stacktrace
+#echo -e "\033[0;32m :datasafe:install --stacktrace \033[0m"
+#./gradlew :datasafe:install --stacktrace
 
 if [ "$CI" == true ] && [ "$TRAVIS_PULL_REQUEST" == false ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
     echo -e "\033[0;32m Bintray upload coming soon \033[0m"
-    # echo -e "\033[0;32m ./gradlew :datasafe-android:bintrayUpload \033[0m"
-    # ./gradlew :datasafe-android:bintrayUpload --stacktrace
+    # echo -e "\033[0;32m ./gradlew :datasafe:bintrayUpload \033[0m"
+    # ./gradlew :datasafe:bintrayUpload --stacktrace
 
 else
     echo -e "\033[0;32m Bintray upload coming soon \033[0m"
