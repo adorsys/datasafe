dc9dfcd15716479d65bd660771335d974637f9e0
Drilon Reçica <drilonrecica.dev@gmail.com>
2019-08-21 20:35:17 +0200

    DAT-150 Update Travis for android project

--- datasafe-android/.buildscripts/run-android-tests.sh
+++ datasafe-android/.buildscripts/run-android-tests.sh
@@ -2,5 +2,16 @@
 
 set +e
 
-echo -e "\033[0;32m ./gradlew app:connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.class=de.adorsys.android.datasafe.DatasafeLogicTest --stacktrace \033[0m"
-./gradlew app:connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.class=de.adorsys.android.datasafe.DatasafeLogicTest --stacktrace
+#echo -e "\033[0;32m ./gradlew app:connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.class=de.adorsys.android.datasafe.DatasafeLogicTest --stacktrace \033[0m"
+#./gradlew app:connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.class=de.adorsys.android.datasafe.DatasafeLogicTest --stacktrace
+
+echo -e "\033[0;32m Android test are coming soon \033[0m"
+
+echo -e "\033[0;32m cd datasafe-android \033[0m"
+cd datasafe-android
+
+echo -e "\033[0;32m ./gradlew  :datasafe:clean --stacktrace \033[0m"
+./gradlew :datasafe:clean --stacktrace
+
+echo -e "\033[0;32m ./gradlew :datasafe:assembleDebug --stacktrace \033[0m"
+./gradlew :datasafe:assembleDebug --stacktrace
