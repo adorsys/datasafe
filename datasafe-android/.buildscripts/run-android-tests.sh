dcbe397bbfd369fa1eff3b7a5c0a7cac5bc9cbfb
Drilon Reçica <drilonrecica.dev@gmail.com>
2019-08-21 20:55:03 +0200

    DAT-150 minor changes to android project

--- datasafe-android/.buildscripts/run-android-tests.sh
+++ datasafe-android/.buildscripts/run-android-tests.sh
@@ -10,7 +10,7 @@ echo -e "\033[0;32m Android test are coming soon \033[0m"
 echo -e "\033[0;32m cd datasafe-android \033[0m"
 cd datasafe-android
 
-echo -e "\033[0;32m ./gradlew  :datasafe:clean --stacktrace \033[0m"
+echo -e "\033[0;32m ./gradlew :datasafe:clean --stacktrace \033[0m"
 ./gradlew :datasafe:clean --stacktrace
 
 echo -e "\033[0;32m ./gradlew :datasafe:assembleDebug --stacktrace \033[0m"
