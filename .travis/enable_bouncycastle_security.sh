ec4babb77bca57af6266126843a7ee55c8bd18af
Valentyn Berezin <valb3r.ua@gmail.com>
2019-08-31 12:05:19 +0300

    Another fix attempt for sed and enabling deploy on custom branch

--- .travis/enable_bouncycastle_security.sh
+++ .travis/enable_bouncycastle_security.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-SED_EXEC="sed"
+# SED flavors in-place replacement handling:
+SED_I_EXEC="sed -i"
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
-    SED_EXEC="gsed"
+    SED_I_EXEC="sed -i ''"
 fi
 
 # 3. Adds BouncyCastle Provider to JDK security providers
@@ -12,10 +13,10 @@ fi
 
 
 # 3. Add BouncyCastle Provider to JDK security providers at place 2 (should replace sun.security.rsa.SunRsaSign)
-$SED_EXEC -i "s/security\.provider\.2=.*/security.provider.2=org.bouncycastle.jce.provider.BouncyCastleProvider/g" "$JAVA_HOME/jre/lib/security/java.security"
+$SED_I_EXEC "s/security\.provider\.2=.*/security.provider.2=org.bouncycastle.jce.provider.BouncyCastleProvider/g" "$JAVA_HOME/jre/lib/security/java.security"
 
 # 4. Add BouncyCastle TLS to JDK security providers at place 3 (should replace sun.security.ec.SunEC and come before com.sun.net.ssl.internal.ssl.Provider)
-$SED_EXEC -i "s/security\.provider\.3=.*/security.provider.3=org.bouncycastle.jsse.provider.BouncyCastleJsseProvider/g" "$JAVA_HOME/jre/lib/security/java.security"
+$SED_I_EXEC "s/security\.provider\.3=.*/security.provider.3=org.bouncycastle.jsse.provider.BouncyCastleJsseProvider/g" "$JAVA_HOME/jre/lib/security/java.security"
 
 # 5. Remove SunEC security provider (if it still exists)
-$SED_EXEC -i "s/security\.provider\..*=sun\.security\.ec\.SunEC//g" "$JAVA_HOME/jre/lib/security/java.security"
+$SED_I_EXEC "s/security\.provider\..*=sun\.security\.ec\.SunEC//g" "$JAVA_HOME/jre/lib/security/java.security"
