2b0ece9dbcdba753214a9d3b046daec8bf2fcb04
Maxim Grischenko <grischenko.maxim@gmail.com>
2019-09-20 13:17:26 +0200

    script takes creds from env vars

--- scripts/runSimpleDatasafeAdapterPerformanceTest.sh
+++ scripts/runSimpleDatasafeAdapterPerformanceTest.sh
@@ -1,24 +1,32 @@
 #!/bin/sh
-if [ $# -eq 0 ]; then
-    echo "No arguments provided"
-    echo "Example usage: sh runSimpleDatasafeAdapterPerformanceTest.sh" \
-    "ACCESS=your_aws_access_key SECRET=your_aws_secret_key BUCKET=bucket_name [URL=ceph_url]"
-    exit 1
-fi
-
-for ARGUMENT in "$@"
-do
-    KEY=$(echo $ARGUMENT | cut -f1 -d=)
-    VALUE=$(echo $ARGUMENT | cut -f2 -d=)
-
-    case "$KEY" in
-            ACCESS)    AWS_ACCESS_KEY=${VALUE} ;;
-            SECRET)    AWS_SECRET_KEY=${VALUE} ;;
-            BUCKET)    AWS_BUCKET=${VALUE} ;;
-            URL)       AWS_URL=${VALUE} ;;
-            *)
-    esac
+ERR=
+REQ_ENVS="AWS_ACCESS_KEY AWS_SECRET_KEY AWS_BUCKET AWS_REGION"
+for ENV in ${REQ_ENVS}; do
+  if [ -z "${!ENV}" ] ; then
+    echo "ERROR: missing ${ENV} environment variable"
+    ERR=1
+  fi
 done
+
+help()
+{
+  echo
+  echo "********************************************************************************************"
+  echo "Script usage:"
+  echo "Before running test script ensures that environment variables are set."
+  echo "List of used variables:"
+  echo "AWS_ACCESS_KEY"
+  echo "AWS_SECRET_KEY"
+  echo "AWS_BUCKET"
+  echo "AWS_REGION"
+  echo "AWS_URL - optional. Used when storage location is not default aws s3 (for example for ceph)"
+  echo "********************************************************************************************"
+}
+
+if [ "$ERR" = "1" ]; then
+  help
+  exit 1
+fi
 
 cd "$(dirname "$0")/../datasafe-long-run-tests/datasafe-business-tests-random-actions/" || exit
 mvn \
@@ -26,8 +34,8 @@ mvn \
 -DAWS_SECRET_KEY="$AWS_SECRET_KEY" \
 -DAWS_BUCKET="$AWS_BUCKET" \
 -DAWS_URL="$AWS_URL" \
+-DAWS_REGION="$AWS_REGION" \
 -DDEFAULT_USER="username" \
 -DDEFAULT_PASSWORD="password" \
--DAWS_REGION="eu-central-1" \
 -DtestArgs="-Xmx256m -DSTORAGE_PROVIDERS="AMAZON" -DFIXTURE_SIZE="SMALL" -DTHREADS=2 -DFILE_SIZES=100" \
 -Dtest=RandomActionsOnSimpleDatasafeAdapterTest test
