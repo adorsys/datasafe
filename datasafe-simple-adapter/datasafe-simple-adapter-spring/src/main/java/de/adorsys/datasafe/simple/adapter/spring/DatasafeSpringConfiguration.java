2b29f518db0b0fdc604d8b1891a32673e4630ec2
Valentyn Berezin <valentyn.berezin@adorsys.com.ua>
2019-09-25 14:17:21 +0300

    DOC-165. Add BouncyCastle for spring bundle too

--- datasafe-simple-adapter/datasafe-simple-adapter-spring/src/main/java/de/adorsys/datasafe/simple/adapter/spring/DatasafeSpringConfiguration.java
+++ datasafe-simple-adapter/datasafe-simple-adapter-spring/src/main/java/de/adorsys/datasafe/simple/adapter/spring/DatasafeSpringConfiguration.java
@@ -1,11 +1,18 @@
 package de.adorsys.datasafe.simple.adapter.spring;
 
 import lombok.extern.slf4j.Slf4j;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.security.Security;
 
 @Configuration
 @ComponentScan(basePackageClasses = DatasafeSpringBeans.class)
 @Slf4j
 public class DatasafeSpringConfiguration {
+
+    public DatasafeSpringConfiguration() {
+        Security.addProvider(new BouncyCastleProvider());
+    }
 }
