3ae2be9f35653b1cf1df2083c66282ef3d1225a5
Valentyn Berezin <valentyn.berezin@adorsys.com.ua>
2019-09-27 13:53:13 +0300

    DOC-159. Test minio only on linux

--- datasafe-cli/src/test/bash/basic_functionality_test_fs.sh
+++ datasafe-cli/src/test/bash/basic_functionality_test_fs.sh
@@ -7,42 +7,16 @@ fi
 
 CLI="$1"
 PROFILE_ROOT="$(pwd)/cli-profiles/"
-MINIO_ACCESS_KEY="minio-access-key"
-MINIO_SECRET_KEY="minio-secret-key"
-MINIO_BUCKET="testBucket"
-VOLUME="$(pwd)/minio"
 SECRET_TEXT="Secret text"
 PRIVATE_ON_FS="folder-fs/secret_on_fs.txt"
 INBOX_ON_FS="hello.txt"
-PRIVATE_ON_MINIO="folder-minio/secret_on_minio.txt"
-
-
-# create minio volume bucket
-mkdir -p "$VOLUME/$MINIO_BUCKET"
-
-# Spin up minio to imitate S3, minio will create MINIO_BUCKET based on volume
-docker run -d \
-  --name MINIO \
-  -p 9000:9000 \
-  -e MINIO_ACCESS_KEY="$MINIO_ACCESS_KEY" \
-  -e MINIO_SECRET_KEY="$MINIO_SECRET_KEY" \
-  --mount type=bind,source="$VOLUME,target=/data" \
-  minio/minio \
-  server /data
 
 do_exit() {
-  # Stop minio
-  echo "Stopping minio"
-  docker rm -f MINIO
   exit "$1"
 }
 
 echo 'Creating profile'
 yes '' | $CLI -u=user -p=password -sp=system_password -rd="$PROFILE_ROOT" profile create || do_exit 1
-
-echo 'Register MINIO storage'
-$CLI -u=user -p=password -sp=system_password -rd="$PROFILE_ROOT" profile storage add -i=MINIO -p="http://127.0.0.1:9000/eu-central-1/$MINIO_BUCKET/" || do_exit 1
-$CLI -u=user -p=password -sp=system_password -rd="$PROFILE_ROOT" profile storage credentials add -m="http://.+" -u="$MINIO_ACCESS_KEY" -p="$MINIO_SECRET_KEY" || do_exit 1
 
 echo "$SECRET_TEXT" > my_secret.txt
 
@@ -66,13 +40,5 @@ FS_INBOX_TEXT=$($CLI -u=friend -p=password-friend -sp=system_password -rd="$PROF
 echo 'Verify FS inbox file list'
 FS_INBOX_LIST=$($CLI -u=friend -p=password-friend -sp=system_password -rd="$PROFILE_ROOT" inbox ls)
 [[ "$FS_INBOX_LIST" != "$INBOX_ON_FS" ]] && echo "Filesystem has wrong shared file list" && exit 1
-
-# Checking how private files work on MINIO:
-echo 'Store secret in MINIO'
-$CLI -u=user -p=password -sp=system_password -rd="$PROFILE_ROOT" private cp -s=MINIO my_secret.txt "$PRIVATE_ON_MINIO" || do_exit 1
-
-echo 'Verify MINIO'
-MINIO_TEXT=$($CLI -u=user -p=password -sp=system_password -rd="$PROFILE_ROOT" private cat -s=MINIO "$PRIVATE_ON_MINIO")
-[[ "$MINIO_TEXT" != "$SECRET_TEXT" ]] && echo "Minio has wrong file content" && do_exit 1
 
 do_exit 0
