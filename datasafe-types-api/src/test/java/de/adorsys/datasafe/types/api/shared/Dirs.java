5c1213781ac35834d749e173a55ded4a46dd53f4
Valentyn Berezin <valentyn.berezin@adorsys.com.ua>
2019-10-07 19:41:47 +0300

    Generify windows path resolve

--- datasafe-types-api/src/test/java/de/adorsys/datasafe/types/api/shared/Dirs.java
+++ datasafe-types-api/src/test/java/de/adorsys/datasafe/types/api/shared/Dirs.java
@@ -2,6 +2,7 @@ package de.adorsys.datasafe.types.api.shared;
 
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
+import org.junit.jupiter.api.condition.OS;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -23,10 +24,19 @@ public class Dirs {
             return walk
                     .filter(it -> !(it.getFileName().startsWith(".") || it.getFileName().startsWith("..")))
                     .filter(it -> !it.equals(root))
-                    .map(it -> root.toUri().relativize(it.toUri()).toString()
-                            .replaceFirst("\\./", "")
-                            .replaceAll("/$", ""))
+                    .map(it -> computeRelative(root, it)
+                    .replaceFirst("\\./", "")
+                    .replaceAll("/$", ""))
                     .collect(Collectors.toList());
         }
+    }
+
+    private String computeRelative(Path root, Path child) {
+        if (!OS.WINDOWS.isCurrentOs()) {
+            return root.toUri().relativize(child.toUri()).toString();
+        }
+
+        // Windows causes double-encoding of URI
+        return root.relativize(child).toString().replace('\\', '/');
     }
 }
