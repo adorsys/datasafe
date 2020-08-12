e84d97dc450b0002bbb2bff6f3f7ef76055440a0
electronicpeter <github@electronicpeter.de>
2020-08-12 12:16:27 +0200

    DOC 175 dependency for await from test to build

--- datasafe-types-api/src/test/java/de/adorsys/datasafe/types/api/shared/AwsClientRetry.java
+++ datasafe-types-api/src/test/java/de/adorsys/datasafe/types/api/shared/AwsClientRetry.java
@@ -1,6 +1,9 @@
 package de.adorsys.datasafe.types.api.shared;
 
 import com.amazonaws.services.s3.AmazonS3;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.awaitility.Duration;
@@ -11,6 +14,20 @@ import static org.awaitility.Awaitility.await;
 public class AwsClientRetry {
     @SneakyThrows
     public static void createBucketWithRetry(AmazonS3 client, String bucket) {
-        await().atMost(Duration.TEN_SECONDS).pollInterval(Duration.ONE_SECOND).untilAsserted(() -> client.createBucket(bucket));
+        FinalCounter counter = new FinalCounter(0);
+        await().atMost(Duration.TEN_SECONDS).pollInterval(Duration.ONE_SECOND).untilAsserted(() -> {
+            log.info("this is the {} try to create bucket", counter.inc().getCounter());
+            client.createBucket(bucket);
+        });
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class FinalCounter {
+        int counter;
+        public FinalCounter inc() {
+            counter++;
+            return this;
+        }
     }
 }
