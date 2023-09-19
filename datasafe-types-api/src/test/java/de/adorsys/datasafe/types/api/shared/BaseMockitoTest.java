1659fa32975ac0cf7843bafaa9c65576260337b9
Maxim Grischenko <maksym.hryshchenko@adorsys.com>
2023-09-19 10:00:28 +0200

    fix test

--- datasafe-types-api/src/test/java/de/adorsys/datasafe/types/api/shared/BaseMockitoTest.java
+++ datasafe-types-api/src/test/java/de/adorsys/datasafe/types/api/shared/BaseMockitoTest.java
@@ -3,27 +3,29 @@ package de.adorsys.datasafe.types.api.shared;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.MockitoAnnotations;
 
 /**
  * This class configures Mockito so that it will inject annotated mocks and display exceptions always
  * at correct places.
  * Tests that use Mockito should use this class.
  */
-@ExtendWith(MockitoExtension.class)
 public abstract class BaseMockitoTest {
+
+    AutoCloseable openMocks;
 
     @BeforeEach
     public void setup() {
+        openMocks = MockitoAnnotations.openMocks(this);
         System.setProperty("SECURE_LOGS", "off");
         System.setProperty("SECURE_SENSITIVE", "off");
     }
 
     @AfterEach
-    public void validate() {
+    public void validate() throws Exception {
         Mockito.validateMockitoUsage();
+        openMocks.close();
     }
 
     @AfterAll
