a65f427767ace60b0991324907f5a88a8b7c0b72
Maxim Grischenko <maksym.hryshchenko@adorsys.com>
2024-01-29 19:06:11 +0100

    release workflow

--- .travis/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "$GPG_SECRET_KEY" | base64 --decode | "$GPG_EXECUTABLE" --import
-echo "$GPG_OWNERTRUST" | base64 --decode | "$GPG_EXECUTABLE" --import-ownertrust
-
-set -e
-
-mvn --settings .travis/settings.xml package gpg:sign deploy -Prelease -DskipTests -B -U;
