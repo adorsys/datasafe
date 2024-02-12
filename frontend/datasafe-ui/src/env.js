cd99d653dd840689f53acb1168607eabb5f0ceca
Borelli-7 <borelli.fotso@adorsys.com>
2024-02-12 18:16:19 +0100

    Remove password commited

--- frontend/datasafe-ui/src/env.js
+++ frontend/datasafe-ui/src/env.js
@@ -1,9 +1,9 @@
 (function (window) {
     window.__env = window.__env || {};
 
-    window.__env.apiUrl = 'http://localhost:8080';
+    window.__env.apiUrl = '${API_URL}';
     // ideally these are not necessary, but API is protected, so supplying it for docker-local deployment
-    window.__env.apiUsername = 'username';
-    window.__env.apiPassword = 'password';
+    window.__env.apiUsername = '${API_USERNAME}';
+    window.__env.apiPassword = '${API_PASSWORD}';
 
 }(this));
