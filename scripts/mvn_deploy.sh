f93e0d4630c9559c98baddc9bf0259a0f317752b
Maxim Grischenko <maksym.hryshchenko@adorsys.com>
2024-06-04 17:08:36 +0200

    fix codecov

--- scripts/mvn_deploy.sh
+++ scripts/mvn_deploy.sh
@@ -5,4 +5,4 @@ set -e
 echo "$GPG_SECRET_KEY" | base64 --decode | $GPG_EXECUTABLE --import --no-tty --batch --yes || true
 echo "$GPG_OWNERTRUST" | base64 --decode | $GPG_EXECUTABLE --import-ownertrust --no-tty --batch --yes || true
 
-mvn clean deploy -ntp --settings scripts/settings.xml gpg:sign -Prelease -DskipTests -U || exit 1
+mvn deploy -ntp --settings scripts/settings.xml gpg:sign -Prelease -DskipTests -U || exit 1
