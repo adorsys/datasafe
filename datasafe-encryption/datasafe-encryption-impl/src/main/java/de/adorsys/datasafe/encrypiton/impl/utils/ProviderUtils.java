9bb5a3b1542fb9c3d8212ec9a6085ec4f6a0f2ad
AssahBismarkabah <assah9799@gmail.com>
2024-07-29 16:58:10 +0100

    update:  pom.xml to ignore de.adorsys.keymanagement:bouncycastle-adapter

--- datasafe-encryption/datasafe-encryption-impl/src/main/java/de/adorsys/datasafe/encrypiton/impl/utils/ProviderUtils.java
+++ datasafe-encryption/datasafe-encryption-impl/src/main/java/de/adorsys/datasafe/encrypiton/impl/utils/ProviderUtils.java
@@ -2,10 +2,8 @@ package de.adorsys.datasafe.encrypiton.impl.utils;
 
 import de.adorsys.keymanagement.adapter.modules.generator.GeneratorModule_ProviderFactory;
 import lombok.experimental.UtilityClass;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import java.security.Provider;
-import java.security.Security;
 
 @UtilityClass
 public class ProviderUtils {
